e4725e867cf0f80b45223a4e04aac7d9825a2bd6
ug-mingyujeon <mingyu.jeon@unitedgames.com>
2025-06-24 12:23:50 +0900

    feat : pretty log

--- aws_kms_sidecar/logger.ts
+++ aws_kms_sidecar/logger.ts
@@ -1,12 +1,23 @@
 import pinoImport from "pino";
-const pino = (pinoImport as any).default ?? pinoImport;
+import prettyImport from "pino-pretty";
 
-const logger = pino({
-  level: process.env.LOG_LEVEL || "info",
-  redact: {
-    paths: ["req.headers.authorization", "AWS_SECRET_ACCESS_KEY", "AWS_ACCESS_KEY_ID"],
-    censor: "[REDACTED]",
+// Handle ESM/CJS interop
+const pino = (pinoImport as any).default ?? pinoImport;
+const pinoPretty = (prettyImport as any).default ?? prettyImport;
+
+const logger = pino(
+  {
+    level: process.env.LOG_LEVEL || "info",
+    redact: {
+      paths: [
+        "req.headers.authorization",
+        "AWS_SECRET_ACCESS_KEY",
+        "AWS_ACCESS_KEY_ID",
+      ],
+      censor: "[REDACTED]",
+    },
   },
-});
+  pinoPretty({ colorize: true, translateTime: "HH:MM:ss" }),
+);
 
 export default logger; 
